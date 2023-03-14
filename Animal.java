a7dc43159edead246c32c6f7c9594a18d7ba25be
y0ungBl0od <vafin.emil.zh@gmail.com>
2023-03-14 22:58:37 +0300

    .....

---
+++ Animal.java
@@ -0,0 +1,16 @@
+public class Animal {
+    String nameAnimal = "Charlie";
+    private String nameAnimal2 = "Markiz";
+    public void eat() {
+        System.out.println("I am eating");
+    }
+    public void sleep(){
+        System.out.println("I am sleeping");
+    }
+    public void showName(){
+        System.out.println(nameAnimal);
+    }
+    public void showName2(){
+        System.out.println(nameAnimal2);
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,10 @@
+public class Dog extends Animal {           // то есть собака нследует класс Animal
+// собака это тоже animal
+// собака наследует класс Animal
+    public void eat(){
+        System.out.println("Dog is eating");
+    }
+    public void bark(){
+        System.out.println("Woof woof");
+    }
+}
